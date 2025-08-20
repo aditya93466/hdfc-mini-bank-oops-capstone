8e5f38ac771006063ad7737db4bba96aab277539
Aditya Arockiadass <142156910+aditya93466@users.noreply.github.com>
2025-08-20 20:51:56 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,12 +1,1 @@
-@echo off
-setlocal enabledelayedexpansion
-if not exist out mkdir out
-for /r %%f in (*.java) do (
-  if not defined files (
-    set files="%%f"
-  ) else (
-    set files=!files! "%%f"
-  )
-)
-javac -d out %files%
-java -cp out com.hdfc.minibank.Main
+
